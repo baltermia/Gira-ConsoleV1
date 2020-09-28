27b9fee93e89e23769832a77cabecba282663cf7
speyck <54800864+speyck@users.noreply.github.com>
2020-09-28 11:17:28 +0200

    feat: getEmployee
    Also added Boolean isAdmin and old code to add createAccount function later

--- Program/src/com/company/employee.java
+++ Program/src/com/company/employee.java
@@ -4,11 +4,26 @@ public class employee {
     int id;
     String username;
     String password;
-    // Todo Admin or User
+    Boolean isAdmin;
 
-    employee(String username, String password) {
+    employee(String username, String password, Boolean admin) {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.isAdmin = admin;
+        manager.employeeList.add(this);
+    }
+
+    public static employee getEmployee(String id) {
+        if (id.matches("[0-9]+")) {
+            for (int i = 0; i < manager.employeeList.size(); i++) {
+                if (Integer.toString(manager.employeeList.get(i).id).equals(id)) return manager.employeeList.get(i);
+            }
+        } else {
+            for (int i = 0; i < manager.employeeList.size(); i++) {
+                if (manager.employeeList.get(i).username.equals(id)) return manager.employeeList.get(i);
+            }
+        }
+        return null;
     }
 }
--- Program/src/com/company/navigation.java
+++ Program/src/com/company/navigation.java
@@ -5,5 +5,52 @@ import java.io.InputStreamReader;
 public class navigation {
     public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-
+    // Create Account:
+    //  void createAccount() throws IOException {
+    //        System.out.println("Bitte geben Sie ihren Vor- und Nachnamen ein (keine zweiten Vornamen): ");
+    //        String name;
+    //        String password;
+    //
+    //        while (true) {
+    //            name = reader.readLine();
+    //            if (name.equals("EXIT")) return;
+    //
+    //            if (!name.equals("")) {
+    //                if (name.contains(" ")) {
+    //                    if (name.charAt(name.length() - 1) != ' ') {
+    //                        if (name.split(" ")[0].length() < 3 || name.split(" ")[1].length() < 3 || name.split(" ")[1] == null) {
+    //                            System.out.println("Ihr Vor- und Nachname müssen mindestens 3 Zeichen lange sein." +
+    //                                    "\nVersuchen Sie es nochmal: ");
+    //                            continue;
+    //                        }
+    //                    } else {
+    //                        System.out.println("Das letzte Zeichen darf kein Leerzeichen sein." +
+    //                                "\nVersuchen Sie es nochmal: ");
+    //                        continue;
+    //                    }
+    //                } else {
+    //                    System.out.println("Ihre Antwort muss mindestens 1 Leerzeichen enthalten." +
+    //                            "\nVersuchen Sie es nochmal: ");
+    //                    continue;
+    //                }
+    //            } else {
+    //                System.out.println("Ihre Antwort darf nicht leer sein." +
+    //                        "\nVersuchen Sie es nochmal: ");
+    //                continue;
+    //            }
+    //            break;
+    //        }
+    //
+    //        System.out.println("Bitte geben Sie ein Passwort ein: ");
+    //
+    //        password = reader.readLine();
+    //        if (password.equals("EXIT")) return;
+    //
+    //        BankAccount account = banking.createAccount(name, password);
+    //
+    //        System.out.println("Account wurde erfolgreich erstellt." +
+    //                "\nIhre Nutzername lautet: " + account.username +
+    //                "\nIhre Account-ID ist: " + account.id +
+    //                "\n-------------------------------");
+    //    }
 }
