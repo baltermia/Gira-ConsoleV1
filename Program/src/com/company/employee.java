1afa6ea3142f3c7cf95d4c865b9f7daa5da7cb40
speyck <54800864+speyck@users.noreply.github.com>
2020-10-19 11:40:21 +0200

    createAccount integrated

--- Program/src/com/company/employee.java
+++ Program/src/com/company/employee.java
@@ -7,23 +7,47 @@ public class employee {
     Boolean isAdmin;
 
     employee(String username, String password, Boolean admin) {
-        this.id = id;
+        id = manager.employeeList.size();
         this.username = username;
         this.password = password;
-        this.isAdmin = admin;
+        isAdmin = admin;
         manager.employeeList.add(this);
     }
 
-    public static employee getEmployee(String id) {
-        if (id.matches("[0-9]+")) {
+    public static employee getEmployee(String id_username) {
+        if (id_username.matches("[0-9]+")) {
             for (int i = 0; i < manager.employeeList.size(); i++) {
-                if (Integer.toString(manager.employeeList.get(i).id).equals(id)) return manager.employeeList.get(i);
+                if (Integer.toString(manager.employeeList.get(i).id).equals(id_username))
+                    return manager.employeeList.get(i);
             }
         } else {
             for (int i = 0; i < manager.employeeList.size(); i++) {
-                if (manager.employeeList.get(i).username.equals(id)) return manager.employeeList.get(i);
+                if (manager.employeeList.get(i).username.equals(id_username)) return manager.employeeList.get(i);
             }
         }
         return null;
+    }
+
+    public static employee createAccount(String name, String password) {
+        String username = (name.substring(0, 3) + name.split(" ")[1].substring(0, 3)).toLowerCase();
+
+        while (true) {
+            if (getEmployee(username) != null) {
+                if (username.length() > 6) {
+                    int strLenth = username.length();
+                    while (strLenth > 0 && Character.isDigit(username.charAt(strLenth - 1))) {
+                        strLenth--;
+                    }
+                    int endNumber = Integer.parseInt(username.substring(strLenth));
+
+                    username = username.substring(0, username.length() - String.valueOf(endNumber).length()) + (endNumber + 1);
+                } else {
+                    username += "1";
+                }
+            } else {
+                break;
+            }
+        }
+        return new employee(username, password, false);
     }
 }
--- Program/src/com/company/navigation.java
+++ Program/src/com/company/navigation.java
@@ -1,56 +1,58 @@
 package com.company;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class navigation {
-    public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     // Create Account:
-    //  void createAccount() throws IOException {
-    //        System.out.println("Bitte geben Sie ihren Vor- und Nachnamen ein (keine zweiten Vornamen): ");
-    //        String name;
-    //        String password;
-    //
-    //        while (true) {
-    //            name = reader.readLine();
-    //            if (name.equals("EXIT")) return;
-    //
-    //            if (!name.equals("")) {
-    //                if (name.contains(" ")) {
-    //                    if (name.charAt(name.length() - 1) != ' ') {
-    //                        if (name.split(" ")[0].length() < 3 || name.split(" ")[1].length() < 3 || name.split(" ")[1] == null) {
-    //                            System.out.println("Ihr Vor- und Nachname müssen mindestens 3 Zeichen lange sein." +
-    //                                    "\nVersuchen Sie es nochmal: ");
-    //                            continue;
-    //                        }
-    //                    } else {
-    //                        System.out.println("Das letzte Zeichen darf kein Leerzeichen sein." +
-    //                                "\nVersuchen Sie es nochmal: ");
-    //                        continue;
-    //                    }
-    //                } else {
-    //                    System.out.println("Ihre Antwort muss mindestens 1 Leerzeichen enthalten." +
-    //                            "\nVersuchen Sie es nochmal: ");
-    //                    continue;
-    //                }
-    //            } else {
-    //                System.out.println("Ihre Antwort darf nicht leer sein." +
-    //                        "\nVersuchen Sie es nochmal: ");
-    //                continue;
-    //            }
-    //            break;
-    //        }
-    //
-    //        System.out.println("Bitte geben Sie ein Passwort ein: ");
-    //
-    //        password = reader.readLine();
-    //        if (password.equals("EXIT")) return;
-    //
-    //        BankAccount account = banking.createAccount(name, password);
-    //
-    //        System.out.println("Account wurde erfolgreich erstellt." +
-    //                "\nIhre Nutzername lautet: " + account.username +
-    //                "\nIhre Account-ID ist: " + account.id +
-    //                "\n-------------------------------");
-    //    }
+    void createAccount() throws IOException {
+        System.out.println("Bitte geben Sie ihren Vor- und Nachnamen ein (keine zweiten Vornamen): ");
+        String name;
+        String password;
+
+        while (true) {
+            name = reader.readLine();
+            if (name.equals("EXIT")) return;
+
+            if (!name.equals("")) {
+                if (name.contains(" ")) {
+                    if (name.charAt(name.length() - 1) != ' ') {
+                        if (name.split(" ")[0].length() < 3 || name.split(" ")[1].length() < 3 || name.split(" ")[1] == null) {
+                            System.out.println("Ihr Vor- und Nachname müssen mindestens 3 Zeichen lange sein." +
+                                    "\nVersuchen Sie es nochmal: ");
+                            continue;
+                        }
+                    } else {
+                        System.out.println("Das letzte Zeichen darf kein Leerzeichen sein." +
+                                "\nVersuchen Sie es nochmal: ");
+                        continue;
+                    }
+                } else {
+                    System.out.println("Ihre Antwort muss mindestens 1 Leerzeichen enthalten." +
+                            "\nVersuchen Sie es nochmal: ");
+                    continue;
+                }
+            } else {
+                System.out.println("Ihre Antwort darf nicht leer sein." +
+                        "\nVersuchen Sie es nochmal: ");
+                continue;
+            }
+            break;
+        }
+
+        System.out.println("Bitte geben Sie ein Passwort ein: ");
+
+        password = reader.readLine();
+        if (password.equals("EXIT")) return;
+
+        employee account = employee.createAccount(name, password);
+
+        System.out.println("Account wurde erfolgreich erstellt." +
+                "\nIhre Nutzername lautet: " + account.username +
+                "\nIhre Account-ID ist: " + account.id +
+                "\n-------------------------------");
+    }
 }
